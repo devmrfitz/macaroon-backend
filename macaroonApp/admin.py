01db66dd2380018bbf1bf6648444a8921a7dceae
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-01-08 20:18:23 +0530

    bugfix

--- macaroonApp/admin.py
+++ macaroonApp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'First_Name', 'Last_Name', 'user', 'public_key')
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
